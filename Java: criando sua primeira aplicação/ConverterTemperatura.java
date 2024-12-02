a0bb186f276c0f615092acb435dcae7482e55a94
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-12-02 10:51:28 -0300

    Update ConverterTemperatura.java

--- Java: criando sua primeira aplicação/ConverterTemperatura.java
+++ Java: criando sua primeira aplicação/ConverterTemperatura.java
@@ -1,1 +1,10 @@
-Text
+public class PrimeiraClasse {
+	public static void main(String[] args) {
+		System.out.println("Converta temperatura de Celsius para Fahrenheit.");
+		
+		int celsius = 37;
+		int f = (int) (celsius * 1.8) + 32;
+		
+		System.out.println("O valor em Fahrenheit é: " + f);	
+	}
+}
