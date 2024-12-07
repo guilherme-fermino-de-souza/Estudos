a3bdec14e32254372e1e68b02d937be63ce98353
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-12-07 14:38:18 -0300

    Update Aula07Banco.java

--- Java: criando sua primeira aplicação/Aula07Banco.java
+++ Java: criando sua primeira aplicação/Aula07Banco.java
@@ -1,1 +1,54 @@
-text
+import java.util.Random;
+import java.util.Scanner;
+
+public class PrimeiraClasse {
+	public  static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		String nome = "Jacqueline Olíveira";
+		String tipoConta ="Corrente";
+		double saldo = 2500;
+		int opcao = 0;
+		String dados = """
+				\nOperações
+				
+				1 - Consultar Saldos
+				2 - Receber Valor
+				3 - Transferir Valor
+				4 - Sair
+				
+				Digite a opção desejada:
+				""";
+
+		System.out.println("********************");
+		System.out.println("\nNome: " + nome);
+		System.out.println("Tipo Conta: " + tipoConta);
+		System.out.println("Saldo: " + saldo);
+		System.out.println("\n********************");
+		
+		while (opcao != 4) {
+			System.out.println(dados);
+			opcao = ler.nextInt();
+			if (opcao == 1) {
+				System.out.println("Seu Saldo é: " + saldo);
+			} else if (opcao == 2) {
+				System.out.println("Inisira o valor que irá receber: ");
+				double valorReceber= ler.nextDouble();
+				saldo += valorReceber;
+				System.out.println("Seu novo saldo é: " + saldo);
+			} else if (opcao == 3) {
+				System.out.println("Inisira o valor que irá transferir: ");
+				double valorTransferir = ler.nextDouble();
+				if (valorTransferir > saldo) {
+					System.out.println("Você não pode transferir pois seu saldo é insuficiente. ");
+				} else {
+				saldo -= valorTransferir;
+				System.out.println("Seu novo saldo é: " + saldo);
+				}
+			} else if (opcao == 4) {
+				System.out.println("Saindo...");
+			} else {
+				System.out.println("Numero inválido.");
+			}
+		}
+	}
+}
