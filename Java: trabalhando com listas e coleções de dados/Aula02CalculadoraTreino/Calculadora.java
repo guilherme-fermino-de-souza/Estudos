3fd840a92400adc9c605576c2286d7face32b115
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-12-08 16:09:11 -0300

    Create Calculadora.java

---
+++ Java: trabalhando com listas e coleções de dados/Aula02CalculadoraTreino/Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+	double numero1, numero2;
+	
+	double Soma() {
+		return numero1 + numero2;
+	}
+	
+	double Subtracao() {
+		return numero1 - numero2;
+	}
+	
+	double Multiplicacao() {
+		return numero1 * numero2;
+	}
+	
+	double Divisao() {
+		return numero1 / numero2;
+	}
+}
