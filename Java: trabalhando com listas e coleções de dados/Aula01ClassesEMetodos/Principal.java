3dba2ea3656c05a5c9a4b018236b8ca93c741b9a
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-12-08 15:05:35 -0300

    Update and rename Java: trabalhando com listas e coleções de dados/Aula00Inicio.java to Java: trabalhando com listas e coleções de dados/Aula01ClassesEMetodos/Principal.java

---
+++ Java: trabalhando com listas e coleções de dados/Aula01ClassesEMetodos/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+	public static void main(String [] arg) {
+		Filme filme1 = new Filme(); //novo objeto
+		filme1.nome = "O Poderoso Chefão";
+		filme1.anoDeLancamento = 1970;
+		filme1.duracaoEmMinutos = 180;
+		
+		filme1.exibeFichaTecnica();
+		filme1.avalia(8);
+		filme1.avalia(5);
+		filme1.avalia(10);		
+		System.out.println(filme1.somaDasAvaliacoes);
+		System.out.println(filme1.totalAvaliacoes);
+		System.out.println(filme1.pegaMedia());
+	}
+}
