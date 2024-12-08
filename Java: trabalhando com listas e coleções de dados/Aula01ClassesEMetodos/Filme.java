3ec4654997015dbb2adebdad39eda9b0562eb5bb
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-12-08 15:06:06 -0300

    Create Filme.java

---
+++ Java: trabalhando com listas e coleções de dados/Aula01ClassesEMetodos/Filme.java
@@ -0,0 +1,23 @@
+public class Filme {
+	String nome;
+	int anoDeLancamento;
+	boolean incluidoNoPlano;
+	double somaDasAvaliacoes;
+	int totalAvaliacoes; 
+	int duracaoEmMinutos;
+	
+	void exibeFichaTecnica() {
+		System.out.println("Nome do filme: " + nome);
+		System.out.println("Ano de lançamento do filme: " + anoDeLancamento);
+		System.out.println("Duração do filme: " +duracaoEmMinutos);
+	}
+	
+	void avalia(double nota) {
+		somaDasAvaliacoes += nota;
+		totalAvaliacoes++;
+	}
+	
+	double pegaMedia() {
+		return somaDasAvaliacoes/totalAvaliacoes;
+	}
+}
