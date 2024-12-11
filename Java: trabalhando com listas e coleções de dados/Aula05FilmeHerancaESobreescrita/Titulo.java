e91190c971d364612a19e764464c22ad7a77ab84
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-12-11 18:46:28 -0300

    Create Titulo.java

---
+++ Java: trabalhando com listas e coleções de dados/Aula05FilmeHerancaESobreescrita/Titulo.java
@@ -0,0 +1,53 @@
+package br.com.alura.screenmatch.modelo;
+
+public class Titulo {
+	private String nome;
+	private int anoDeLancamento;
+	private boolean incluidoNoPlano;
+	private double somaDasAvaliacoes;
+	private int totalAvaliacoes; 
+	private int duracaoEmMinutos;
+	
+	public int getTotalDeAvaliacoes() {
+		return totalAvaliacoes;
+	}
+	
+	public void setTotalDeAvaliacoes(int totalAvaliacoes) {
+		this.totalAvaliacoes = totalAvaliacoes;
+	}
+	
+	public void setAnoDeLancamento(int anoDeLancamento) {
+		this.anoDeLancamento = anoDeLancamento;
+	}
+	
+	public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+		this.incluidoNoPlano = incluidoNoPlano;
+	}
+	
+	public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+		this.duracaoEmMinutos = duracaoEmMinutos;
+	}
+	public int getDuracaoEmMinutos() {
+		return duracaoEmMinutos;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome; 
+	}
+	
+	public void exibeFichaTecnica() {
+		System.out.println("Nome do filme: " + nome);
+		System.out.println("Ano de lançamento do filme: " + anoDeLancamento);
+		System.out.println("Duração do filme: " +duracaoEmMinutos);
+		System.out.println("Incluido no plano: " +incluidoNoPlano);
+	}
+	
+	public void avalia(double nota) {
+		somaDasAvaliacoes += nota;
+		totalAvaliacoes++;
+	}
+	
+	public double pegaMedia() {
+		return somaDasAvaliacoes/totalAvaliacoes;
+	}
+}
