09f32abcdf95d5e767a44dae4b9d04370f935431
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-12-30 12:28:55 -0300

    Create main.py

---
+++ Python/Youtube Bro Code/Aula03-TypeCasting/main.py
@@ -0,0 +1,16 @@
+# Typecasting = the process of coverting a variable from one data type to another
+# str(),int(), float() , bool()
+
+name = "Guilherme"
+age = 17
+gpa = 3.2
+is_student =True
+
+# print(type(age))
+
+age = str(age)
+age += "1"
+print(age)
+
+name = bool(name) #return False if the String is empty
+print(name)
