ba223a9d43a3051f38acde0eff88b7c4f09f26c2
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-12-30 12:56:12 -0300

    Create exercise01.py

---
+++ Python/Youtube Bro Code/Aula04-input/exercise01.py
@@ -0,0 +1,8 @@
+# Exercise 01 Rectancle Area Calc
+
+print("Rectancle Area Calc")
+length = float(input("Enter the Length "))
+width = float(input("Enter the Width "))
+area = length * width
+
+print(f"The rectangle area is: {area}cm²")
