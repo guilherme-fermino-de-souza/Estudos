a3d8d9aca718ee188dc774564fe88ca76c81f085
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-01-06 17:37:29 -0300

    Create ProdutoPerecivel.java

---
+++ Java: trabalhando com listas e coleções de dados/Aula11TreinoFinal/ProdutoPerecivel.java
@@ -0,0 +1,13 @@
+
+public class ProdutoPerecivel extends Produto{
+	private int dataValidade;
+	
+	public ProdutoPerecivel(String nome, double preco, int quantidade, int dataValidade) {
+		super(nome, preco,quantidade);
+		this.dataValidade = dataValidade;
+	}
+
+	public int getDataValidade() {
+		return dataValidade;
+	}
+}
