c8c12bd23b611d835582713ee1e879b509f08072
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-01-09 17:06:33 -0300

    Create Aula06Math.py

---
+++ Python/Youtube Bro Code/Aula06Math.py
@@ -0,0 +1,11 @@
+x = 3.14
+y = 4
+z = 5
+
+# result = round(x) //Arredondar
+#result = abs(y) //Distance from Zero
+#result = pow(y,z) //Potenciação
+#result = max(x , y, z)
+#result = min(x , y, z)
+
+print(result)
