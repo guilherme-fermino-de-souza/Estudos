38f614e09e844e34cb29493925bab1cb5dc9a75f
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-01-13 18:25:33 -0300

    Create Animal.java

---
+++ Java: trabalhando com listas e coleções de dados/Aula12 - ArrayLists, casting de objeto e instanceof/Atividades/atv01/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+	private String nome;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public String toString() {
+		return this.getNome();
+	}
+}
