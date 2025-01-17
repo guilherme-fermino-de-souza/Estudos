36ac835a0121a717d01c5bcdd65478bfe483c803
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-01-17 19:06:21 -0300

    Create Filme.java

---
+++ Java: trabalhando com listas e coleções de dados/Aula12 - ArrayLists, casting de objeto e instanceof/Ordenando Dados/Filme.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch.modelo;
+
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel{
+	private String diretor;
+	
+	public Filme(String nome, int anoDeLancamento) { //Sem retorno; Nome da classe
+		super(nome, anoDeLancamento);
+		this.setNome(nome);
+		this.setAnoDeLancamento(anoDeLancamento);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getDiretor() {
+		return diretor;
+	}
+	
+	public void setDiretor(String diretor) {
+		this.diretor = diretor;
+	}
+	
+	@Override
+	public int getClassificacao() {
+		return (int) pegaMedia() / 2;
+	}
+	
+	@Override
+	public String toString() {
+		return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
+	}
+}
