3256e15153ddfa597a28ba963a84f5d6aed38cec
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-01-17 14:29:00 -0300

    Create temp.py

---
+++ Python/Youtube Bro Code/Aula - Logical operators/temp.py
@@ -0,0 +1,22 @@
+# Logical operators = evaluate multiple conditions (or, and, not)
+
+temp = 20
+is_sunny = False
+if temp >= 28 and is_sunny:
+    print("It is HOT outside 🥵")
+    print("It is SUNNY🟡")
+elif temp <= 0 and is_sunny:
+    print("It is COLD outside🥶")
+    print("It is SUNNY🟡")
+elif 28 > temp > 0 and is_sunny:
+    print("It is WARM outside😀")
+    print("It is SUNNY🟡")
+elif temp >= 28 and not is_sunny:
+    print("It is HOT outside 🥵")
+    print("It is CLOUDY⚪")
+elif temp > 0 and not is_sunny:
+    print("It is COLD outside🥶")
+    print("It is CLOUDY⚪")
+elif 28 > temp > 0 and not is_sunny:
+    print("It is WARM outside😀")
+    print("It is CLOUDY⚪")
