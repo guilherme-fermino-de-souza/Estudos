97ba3a27c0244b9d231c44fcb2f208a6956de79b
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-01-26 16:35:41 -0300

    Create StringIndexing.py

---
+++ Python/Youtube Bro Code/Aula08 - String Indexing/StringIndexing.py
@@ -0,0 +1,11 @@
+# indexing = acessing elements of a sequence using [] (indexing operator)
+#   [Start : end : step]
+
+credit_number = "1234-5678-9012-3456"
+
+print(credit_number[0])
+print(credit_number[:10])
+print(credit_number[5:10])
+print(credit_number[5:])
+print(credit_number[-7]) #Conta ao contrário
+print(credit_number[::2]) #Pula charachteres
