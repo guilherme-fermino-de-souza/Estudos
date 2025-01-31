0bd71b1e0a37d1bc59a13d914194d03d672d0443
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-01-31 14:01:39 -0300

    Update Set { } - .py

--- Python/Youtube Bro Code/Aula11 - Collections/Set { } - .py
+++ Python/Youtube Bro Code/Aula11 - Collections/Set { } - .py
@@ -1,1 +1,17 @@
-tect
+# collection = single variavle used to store multiple values
+#    List = [] ordered and changeable. Duplicates OK
+#    Set = {} unordered and immutable. but Add/Remove OK. NO Duplicates
+#    Tuple = () ordered and unchangeable. Duplicates OK. FASTER
+
+fruits = {"Apple", "orange", "banana", "coconut"}
+# print(dir(fruits)) //Show all the diferent atributes and methods
+# print(help(fruits)) //Documentation of the atributes and methods
+# print(len(fruits))
+# print("pineapple" in fruits) //True or False
+
+fruits.add("pineapple")
+fruits.remove("Apple")
+# fruits.clear()
+# For Loop
+for fruit in fruits:
+    print(fruit)
